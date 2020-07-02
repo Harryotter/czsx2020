0c26619bf1b8744896e48f0a591f226395fabca8
hermione101 <hermione101@163.com>
2020-07-02 11:32:58 +0800

    搜索结果页面的分页方法

--- HanChineseClothing/src/main/java/org/example/p02service/ClothService.java
+++ HanChineseClothing/src/main/java/org/example/p02service/ClothService.java
@@ -50,4 +50,37 @@ public class ClothService {
 
 
     }
+
+    public PageBean<Cloth> search(String keyword,int pageSize,int currentPage) throws Exception {
+     //创建分页对象
+        PageBean<Cloth> pageBean=new PageBean<Cloth>();
+        //设置每页记录数  1
+        pageBean.setPageSize(pageSize);
+        //当前所在页  2
+        pageBean.setCurrentPage(currentPage);
+/**
+* PageBean中的其他成员对象通过Dao查询：
+* 总记录数、tab_cloth中的总共记录条数、分割后的页数
+*/
+    //创建Dao对象
+        ClothDao clothDao = new ClothDao();
+        //查询总记录数  3
+        int count=clothDao.findCountByKeyword(keyword);
+        pageBean.setTotalCount(count);
+        //计算所分割后的页面  4
+        int totalpage;
+        if(count%pageSize==0){
+            totalpage=count/pageSize;
+        }else{
+            totalpage=count/pageSize+1;
+        }
+        pageBean.setTotalPage(totalpage);
+
+        //设置当前页面的集合  5
+        List<Cloth> list=clothDao.findPageByKeyword(keyword,pageSize,currentPage);
+        pageBean.setList(list);
+    //返回分页对象
+        return pageBean;
+
+    }
 }
